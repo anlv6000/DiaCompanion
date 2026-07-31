af8cdcfb0f098d1d177b6a03b8867f9f89cb4760
quandxnunxi28 <quandxnunxi28@gmail.com>
2026-07-31 19:15:07 +0700

    remove duplicate DTOs folder and keep Dtos

--- Backend/DiaCompanion/DTOs/DoctorReplyRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-using DiaCompanion.Api.Common;
-
-namespace DiaCompanion.Api.Dtos;
-
-public class DoctorReplyRequest
-{
-    [Required, MaxLength(1000)] public string Reply { get; set; } = "";
-}
